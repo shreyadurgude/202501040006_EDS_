32355655187b181ff815563e89c719cba7c185d2
shreyadurgude <durgudeshreya9@gmail.com>
2026-05-03 19:43:27 +0530

    Create 1.1.3.py

---
+++ Practical 1.py/1.1.3.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+date1 = input()
+date2 = input()
+d1 = datetime.strptime(date1, "%Y-%m-%d")
+d2 = datetime.strptime(date2, "%Y-%m-%d")
+difference = d2 - d1
+print(difference.days)
